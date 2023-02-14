8b2dc0bdb75654892ac0704db19aa416e5c70402
bob <bob@gmail.com>
2023-02-14 10:43:13 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Adicionando um novo Texto</Text>
+      <Text>Adicionando um novo Cabecalho</Text>
+      <Button title="Enviar"></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
